17d907290b1ea3f6a7c7e8ef4e976f931114e8d6
gs-akhan <akhan@gainsight.com>
2014-06-06 22:02:00 +0530

    Added logger middleware to log all the requests

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var connect = require('connect'),
 //You need to call next() because the control in then moved to exceute another middleware
 //Else the /home will never be excuted.
 
+//Adding connect logger middleware
+
+app.use(connect.logger());
 app.use(function(req, res, next) {
 	console.log(req.url);
 	next();
