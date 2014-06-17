93bfdab2aa51ecba686a7affc73f7377056c9b1f
gs-akhan <akhan@gainsight.com>
2014-06-17 11:38:59 +0530

    reported the app to express

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
-var express = require('express'),
-	connect = require('connect');
 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
 var app = express();
-app.use(connect.cookieParser('mysecretkey'));
-app.use(function(req, res, next) {
-	next();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-//addingd static middleware
-app.use(connect.directory('public', {icons:true}));
-app.use(connect.static(__dirname+'/public'));
-app.use(connect.session());
-
-app.get('/home', function(req, res, next) {
-	
-	var sess = req.session;
-	res.statusCode = 200;
-	res.setHeader('Content-Type','text/html');
-	res.setHeader('Set-Cookie', "name=khan");
-	if(!sess.views) {
-		sess.views = 1;
-		sess.name = " Mr.Connect ";
-		res.end('<h1>Hello world, this is your home</h1>');
-		
-	}
-	else {
-		res.end('<h1>Hello '+sess.name+', this is your home '+ req.session.views+'</h1>');	
-		sess.views++;	
-	}
-});
-
-//Middleware for error handlers
-app.use(errorHandler());
-
-app.listen(4000, function() {
-	console.info('App running on port 127.0.0.1:4000');
-});
-
-function errorHandler() {
-
-	return function (err, req, res, next) {
-		res.statusCode = 500;
-		res.end("<h1>Internal Server Error</h1>");
-	};
-
-};
